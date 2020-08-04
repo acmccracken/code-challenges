1042d26decfb187c7de01897e371c594835cc7e2
Alan McCracken <acmccracken@gmail.com>
2020-08-04 14:08:23 -0500

    uno mas

--- challenges3.js
+++ challenges3.js
@@ -87,3 +87,22 @@ var searchBST = function(root, val) {
         }
     }return null;
 };
+
+var isPowerOfFour = function(num) {
+    if(num === 1 || num === 4){
+        return true;
+    }
+    if(num <= 0){
+        return false;
+    }
+    while(num > 0){
+        if(num % 4 === 0){
+            num /= 4;
+            if (num === 4){
+                return true;
+            }
+        }else{
+            return false;
+        }
+    }
+};
