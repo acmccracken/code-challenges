c47a9f276fa29d792c49c83f23175792d9616c95
Alan McCracken <acmccracken@gmail.com>
2020-08-08 07:07:51 -0500

    update

--- challenges3.js
+++ challenges3.js
@@ -106,3 +106,48 @@ var isPowerOfFour = function(num) {
         }
     }
 };
+
+var rob = function(nums) {
+    // initiate first two values of dp array
+    if (nums.length === 0) return 0;
+    if (nums.length === 1) return nums[0]
+    
+    let dp = [nums[0], Math.max(nums[0], nums[1])];
+    console.log(dp);
+    
+    for (let i = 2; i < nums.length; i++) {
+        // compare current max with the previous max
+        dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+    }
+    return dp[nums.length - 1];
+}
+
+
+//doesn't work, saving for reference
+var longestValidParentheses = function(s) {
+    let cache = [0, 0, 0];
+    for(let i = 0; i < s.length; i ++){
+        if(s.charAt(i) == '('){
+            cache[2]++;
+            if((cache[2] > 1) && (cache[1] > 1)){
+                cache[2] -= 2;
+            }
+           
+        }else{
+            cache[2]--;
+            if(cache[2] < 0){
+                cache[2] ++;
+                cache[1] = 0;
+                
+            }else{
+                cache[1] += 2;
+                if (cache[1] > cache[0]){
+                    cache[0] = cache[1];
+                }
+            }
+        }
+        console.log(cache)
+    }
+    return cache[0];
+    
+};
