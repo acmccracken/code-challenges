0d0b973a48f82a54c0066e15678ee6f86505ea5b
Alan McCracken <acmccracken@gmail.com>
2020-09-04 13:10:23 -0500

    isPalindrome and unshift

--- SpecificChallenges/problemSolving/recursion.js
+++ SpecificChallenges/problemSolving/recursion.js
@@ -26,6 +26,36 @@ function reverse(str){
     
   }
   
-console.log(reverse('awesome')) // 'emosewa'
-console.log(reverse('rithmschool')) // 'loohcsmhtir'
+// console.log(reverse('awesome')) // 'emosewa'
+// console.log(reverse('rithmschool')) // 'loohcsmhtir'
+
+
+/* Write a recursive function called isPalindrome which returns true if athe string passed through is a palindrome
+   Otherwise it returns false
+*/
+
+function isPalindrome(str){
+    let arr = str.split('');
+    function recFunc(arr){
+        if(arr.length <= 1){
+            //I couldn't get it to return true here, not sure why
+            return;
+        }else if(arr[0]  === arr[arr.length - 1]){
+            arr.pop();
+            arr.shift();
+            recFunc(arr);
+        }else{
+            return;
+        }
+    }
+    recFunc(arr);
+    if(arr.length <= 1){
+        return true;
+    }else return false;
+}
+
+// console.log(isPalindrome('awesome')) //false
+// console.log(isPalindrome('foobar')) //false
+// console.log(isPalindrome('tacocat')) //true
+// console.log(isPalindrome('taccat')) //true
 
--- SpecificChallenges/problemSolving/singlyLinkedLists.js
+++ SpecificChallenges/problemSolving/singlyLinkedLists.js
@@ -47,6 +47,18 @@ class SinglyLinkedList{
         this.length --;
         return currentHead;
     }
+    unshift(val){
+        var newNode = new Node(val);
+        if(!head){
+            this.head = newNode;
+            this.tail = this.head;
+        }else{
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
 }
 
 var list = new SinglyLinkedList()
