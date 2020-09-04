0234f7315d3038e7968011f67012f20e43a08467
Alan McCracken <acmccracken@gmail.com>
2020-09-04 17:25:04 -0500

    stuck on nestedEvenSum

--- SpecificChallenges/problemSolving/recursion.js
+++ SpecificChallenges/problemSolving/recursion.js
@@ -59,3 +59,60 @@ function isPalindrome(str){
 // console.log(isPalindrome('tacocat')) //true
 // console.log(isPalindrome('taccat')) //true
 
+function capitalizeFirst (arr) {
+    let num = 0;
+    function iteration(num){
+        arr[num] = arr[num].split('')
+        arr[num][0] = arr[num][0].toUpperCase()
+        arr[num] = arr[num].join('');
+        if(num < (arr.length - 1)) iteration(num+1);
+        else return;
+        
+    }
+    iteration(num)
+    return arr;
+}
+
+// console.log(capitalizeFirst(['car', 'taco', 'banana']))
+
+//stuck a little, still figuring ot what's not working
+function nestedEvenSum(obj) {
+    let valArr = Object.values(obj);
+    let num = 0;
+    let total = 0;
+    console.log(valArr)
+    while(num <= valArr.length){
+        if(typeof(valArr[num]) === Object){
+            nestedEvenSum(val[num]);
+        }else if(valArr[0] % 2 === 0){
+            total += valArr[0];
+            console.log(valArr)
+        }
+        num++;
+        console.log(total)
+    }
+    return total;
+}
+
+var obj1 = {
+    outer: 2,
+    obj: {
+      inner: 2,
+      otherObj: {
+        superInner: 2,
+        notANumber: true,
+        alsoNotANumber: "yup"
+      }
+    }
+  }
+  
+  var obj2 = {
+    a: 2,
+    b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+    c: {c: {c: 2}, cc: 'ball', ccc: 5},
+    d: 1,
+    e: {e: {e: 2}, ee: 'car'}
+  };
+  
+  console.log(nestedEvenSum(obj1)); // 6
+  console.log(nestedEvenSum(obj2)); // 10
