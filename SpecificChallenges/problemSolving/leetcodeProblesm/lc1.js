6d9cd36a257be67f59d99697ec155a18e5bec8d8
Alan McCracken <acmccracken@gmail.com>
2020-09-14 09:15:10 -0500

    daily challenge

--- SpecificChallenges/problemSolving/leetcodeProblesm/lc1.js
+++ SpecificChallenges/problemSolving/leetcodeProblesm/lc1.js
@@ -47,4 +47,41 @@ var myAtoi = function(str){
 //     return(numArr);
 // };
 
-console.log(myAtoi('ddd333ddf356word dss ddd'))
+//console.log(myAtoi('ddd333ddf356word dss ddd'))
+
+var isPalindrome = function(x) {
+    let bool = false;
+    x = x.toString().split('');
+    console.log(x)
+    function checkPalindrome(x){
+        if(x[0] === x[x.length-1]){
+            x.pop();
+            x.shift();
+            if(x.length <= 1) bool = true;
+            else checkPalindrome(x);
+        }else bool = false;
+        
+    }
+    checkPalindrome(x);
+    return bool
+};
+
+
+//console.log(isPalindrome(1211))
+
+
+//multiple pointers
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length-1;
+    let max = 0;
+    while(left < right){
+        max = Math.max((right - left) * Math.min(height[left], height[right]), max);
+        if (height[left] <= height[right]) left++;
+        else right --;
+    }
+    return max
+};
+
+//console.log(maxArea([1,8,6,2,5,4,8,3,7]))
+
